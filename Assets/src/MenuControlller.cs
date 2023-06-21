25d279acf4e7f2aad448510d89b5236c286a65b3
Ivan Kosov <kosov.vanya@gmail.com>
2023-06-20 20:16:22 -0500

    Choose difficulties

--- Assets/src/MenuControlller.cs
+++ Assets/src/MenuControlller.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuControlller : MonoBehaviour
 {
     public Settings Settings;
 
-    public void SmallField()
+    public Slider Slider;
+
+    private void CreateGame(int width, int height)
     {
-        Settings.Width = 15;
-        Settings.Height = 10;
-        Settings.Mines = 20; // TODO remove
+        Settings.Width = width;
+        Settings.Height = height;
+        Settings.Mines = (int)(width * height * Slider.value);
 
         SceneManager.LoadScene("Game");
+    }
+
+    public void SmallField()
+    {
+        CreateGame(15, 10);
+    }
+
+    public void MediumField()
+    {
+        CreateGame(25, 20);
+    }
+
+    public void BigField()
+    {
+        CreateGame(35, 25);
     }
 }
